0777ec84ce4b112d310354978b3ccbd122c7ef72
Adam Lloyd <alloyddesign@gmail.com>
2020-04-19 17:49:35 +0100

    fix for heroku

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  host: process.env.HOST || 'localhost',
   port: process.env.PORT || '5000',
   playerTimeout: process.env.PLAYER_TIMEOUT || 1000 * 60,
   defaultQuestionTime: process.env.QUESTION_TIME || 1000 * 30,
