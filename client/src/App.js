a0a594ed1d76d6594bf702dc7bbb69f91bb22c80
Adam Lloyd <alloyddesign@gmail.com>
2020-04-26 21:46:55 +0100

    header and fixing some spring bugs

--- client/src/App.js
+++ client/src/App.js
@@ -16,9 +16,9 @@ function App() {
         className="overflow-hidden h-full flex flex-col text-lg relative border-l-0 border-r-0 border-gray-400
       bg-gray-200 md:border-r-2 md:border-l-2 text-2xl font-semibold text-gray-800"
       >
-        <div className="tracking-widest flex flex-row w-full absolute text-2xl font-light flex-grow-0 px-4 mt-2 text-gray-100 z-10">
+        <div className="tracking-widest flex flex-row w-full justify-center items-center absolute text-2xl font-light flex-grow-0 px-4 mt-2 text-gray-100 z-10">
           <span className="flex flex-grow">Quiz!</span>
-          <span>{baseUrl}/test</span>
+          <span className="text-xl">{baseUrl}/test</span>
         </div>
         <ErrorContainer />
         <div className="wave" />
